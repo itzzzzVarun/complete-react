8930c46b454835cea47748320f0cca21e6af0841
ItzzzzVarun <itzzzzVarun>
2024-10-03 22:22:32 +0530

    Woking on social media project

--- projects/social-media/src/store/post-list-store.jsx
+++ projects/social-media/src/store/post-list-store.jsx
@@ -2,7 +2,7 @@ import {createContext, useReducer } from "react";
 
 
 
-const PostList = createContext(
+export const PostList = createContext(
   {
     postList : [],
     addPost : () => {},
@@ -12,18 +12,34 @@ const PostList = createContext(
 
 const postListReducer = (currPostList, action) => {
   let newPostList = currPostList
+  if (action.type === 'DELETE_POST') {
+    
+    newPostList = currPostList.filter((post) => 
+      post.id !== action.payload.postId
+    )
+  }
   return newPostList
 } 
 
 const PostListProvider = ({ children }) => {
 
-  const [postList, dispatchPostList] = useReducer(postListReducer, [])
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer, 
+    DEFAULT_POST_LIST
+  )
+
   const addPost = () => {
 
   }
   
-  const deletePost = () => {
+  const deletePost = (postId) => {
 
+    
+    dispatchPostList({
+      type: 'DELETE_POST',
+      payload : {
+        postId
+      }})
   }
 
     return (
@@ -32,5 +48,22 @@ const PostListProvider = ({ children }) => {
       </ PostList.Provider>
     )
 }
+
+const DEFAULT_POST_LIST = [{
+  id: '1',
+  title: 'going to mumbai',
+  body: 'Hii, Friends i am going to mumbai',
+  reactions: 3,
+  userId: 'user-9',
+  tags: ['vacation', 'mumbai', 'enjoy']
+},
+{
+id: '2',
+title: 'kyo hue pass',
+body: 'kya ab abhi quiz nhi hogi',
+reactions: 0,
+userId: 'user-dead',
+tags: ['L lg gye', 'not placed', 'sucide']
+}]
 
 export default PostListProvider
